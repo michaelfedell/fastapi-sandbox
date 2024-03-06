b8d16e5aa7f5c6fbccd8dcebcd070bffb8a2b304
Michael Fedell <michael.fedell@vastspace.com>
2024-03-06 12:04:45 -0800

    feat: add second bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
 resource "aws_s3_bucket" "sandbox" {
 
 }
+resource "aws_s3_bucket" "spacelift" {
+  bucket = "mfedell-spacelift-demo"  
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.sandbox.bucket
 }
+
+output "demo_bucket" {
+  value = aws_s3_bucket.spacelift.bucket
+}
